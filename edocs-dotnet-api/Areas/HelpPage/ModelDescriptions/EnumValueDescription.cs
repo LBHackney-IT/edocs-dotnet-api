0c35a3c3d21036236ab14277776c94726eb872a2
Ninamma <32230328+Ninamma@users.noreply.github.com>
2020-04-21 16:29:35 +0100

    trying simple endpoint

---
+++ edocs-dotnet-api/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace edocs_dotnet_api.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
